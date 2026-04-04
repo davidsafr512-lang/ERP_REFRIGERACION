267f57161c14f3f74f42c0763661752e2ba6141c
davidsafr512-lang <davidsafr512@gmail.com>
2026-04-04 14:30:33 -0400

    Script para auto-creación de superusuario con contraseña

--- create_admin.py
+++ create_admin.py
@@ -9,10 +9,9 @@ from django.contrib.auth import get_user_model
 
 def create_superuser():
     User = get_user_model()
-    # Cambia estos datos por los que quieras usar
     username = 'admin_pollo'
     email = 'admin@pollopremium.com'
-    password = 'TuPasswordSeguro123' # Pon una clave real aquí
+    password = 'clara530'
 
     if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(username, email, password)
