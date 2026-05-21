0697304b0f67b7c1f4da152ca4b442458a92d82a
davidsafr512-lang <davidsafr512@gmail.com>
2026-05-20 20:12:17 -0400

    Registro simplificado de usuario para error 500

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import Usuario # El nombre exacto de tu modelo
 
 # Esto le dice a Django: "Muestra mi usuario en el panel de control"
-admin.site.register(Usuario, UserAdmin)
+admin.site.register(Usuario)
